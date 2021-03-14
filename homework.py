440972ff3ef7dbf9232cf233df683a371e53c187
Ruize Zhang <33107657+RuizeZ@users.noreply.github.com>
2021-03-14 19:45:12 +0800

    piece knows where valid moves are and how to remove pieces

--- homework.py
+++ homework.py
@@ -16,7 +16,145 @@ class Boardinfo:
             else:
                 gameBoard[newrow][newcol] = "W"
                 self.whiteKingPiece += 1
-        output(gameBoard)
+        
+    
+    def getValidMoves(self, color, isKing, oldrow, oldcol):
+        leftDiagonal = oldcol - 1
+        rightDiagonal = oldcol + 1
+        validMoves = []
+        if color == "b" or isKing:
+            validMoves += (self.leftSide(oldrow + 1, min(oldrow + 3, 8), 1, color, leftDiagonal))
+            validMoves += (self.rightSide(oldrow + 1, min(oldrow + 3, 8), 1, color, rightDiagonal))
+            print("valid moves")
+            print(validMoves)
+        if color == "w" or isKing:
+            validMoves += (self.leftSide(oldrow - 1, max(oldrow - 3, -1), -1, color, leftDiagonal))
+            validMoves += (self.rightSide(oldrow - 1, max(oldrow - 3, -1), -1, color, rightDiagonal))
+            print("valid moves")
+            print(validMoves)
+        return validMoves
+
+    def leftSide(self, startrow, endrow, direction, color, left, jumpNode=[]):
+        # print("in rightSide")
+        print("left jumpNode: ", jumpNode)
+        validMoves = []
+        previous = []
+        
+        if startrow > 7  or startrow < 0:
+            return validMoves
+        for i in range(startrow, endrow, direction):
+            print("prev", previous)
+            if left < 0:
+                break
+            print("[i][left]: ", i, left)
+            if self.gameBoard[i][left].lower() == color:
+                print("same color")
+                break
+            
+            #current is a empty square
+            elif self.gameBoard[i][left] == '.': 
+                #after one jumpNode, left is . 
+                if jumpNode and not previous:
+                    break
+                #after one jumpNode, left is another color
+                elif jumpNode:
+                    pass
+                    # validMoves[(i, left)] = previous + jumpNode
+                #didnot jumpNode at all
+                elif not previous and not jumpNode:
+                    finalPosition = [i, left]
+                    finalPosition.append(jumpNode)
+                    validMoves.append(finalPosition)
+                #it is possible to jumpNode
+                if previous:
+                    jumpNode.append(previous)
+                    validMovesLength = len(validMoves)
+                    if direction == -1:
+                        next_endrow = max(i - 3, -1)
+                    else:
+                        next_endrow = min(i + 3, 8)
+                    validMoves += (self.leftSide(i + direction, next_endrow, direction, color, left - 1, jumpNode))
+                    print("validMoves: ", validMoves)
+                    print("jumpNode: ", jumpNode)
+                    validMoves += (self.rightSide(i + direction, next_endrow, direction, color, left + 1, jumpNode))
+                    print("validMoves: ", validMoves)
+                    print("jumpNode: ", jumpNode)
+                    if len(validMoves) == validMovesLength:
+                        finalPosition = [i, left]
+                        new_jumpNode = jumpNode.copy()
+                        finalPosition.append(new_jumpNode)
+                        validMoves.append(finalPosition)
+                        print("validMoves.append(finalPosition): ",validMoves)
+                    jumpNode.pop(-1)
+                break
+            #it is another color, and it is possible to jumpNode
+            else:
+                previous = [i, left]
+            left -= 1
+        return validMoves
+
+    def rightSide(self, startrow, endrow, direction, color, right, jumpNode=[]):
+        print("in rightSide")
+        print("right jumpNode: ", jumpNode)
+        validMoves = []
+        previous = []
+        
+        if startrow > 7  or startrow < 0:
+            return validMoves
+        for i in range(startrow, endrow, direction):
+            print("prev", previous)
+            if right > 7:
+                break
+            print("[i][right]: ", i, right)
+            if self.gameBoard[i][right].lower() == color:
+                print("same color")
+                break
+            
+            #current is a empty square
+            elif self.gameBoard[i][right] == '.': 
+                #after one jumpNode, right is . 
+                if jumpNode and not previous:
+                    break
+                #after one jumpNode, right is another color
+                elif jumpNode:
+                    # validMoves[(i, right)] = previous + jumpNode
+                    pass
+                elif not previous and not jumpNode:
+                    finalPosition = [i, right]
+                    finalPosition.append(jumpNode)
+                    validMoves.append(finalPosition)
+                print("in right validMoves: ", validMoves)
+                #it is possible to jumpNode
+                if previous:
+                    jumpNode.append(previous)
+                    validMovesLength = len(validMoves)
+                    if direction == -1:
+                        next_endrow = max(i - 3, -1)
+                    else:
+                        next_endrow = min(i + 3, 8)
+                    validMoves += (self.rightSide(i + direction, next_endrow, direction, color, right + 1, jumpNode))
+                    validMoves += (self.leftSide(i + direction, next_endrow, direction, color, right - 1, jumpNode))
+                    if len(validMoves) == validMovesLength:
+                        finalPosition = [i, right]
+                        new_jumpNode = jumpNode.copy()
+                        finalPosition.append(new_jumpNode)
+                        validMoves.append(finalPosition)
+                        print("validMoves.append(finalPosition): ",validMoves)
+                    jumpNode.pop(-1)
+                break
+            #it is another color, and it is possible to jumpNode
+            else:
+                previous = [i, right]
+            right += 1
+        return validMoves
+    
+    def remove(self, removePieces):
+        newlist = removePieces[2:][0]
+
+        print(newlist)
+        for piece in newlist:
+            print(piece)
+            gameBoard[piece[0]][piece[1]] = '.'
 
 class during_game:
     def __init__(self, gameBoard, color):
@@ -25,6 +163,7 @@ class during_game:
         self.validMoves = []
         self.selected = None
         self.color = color
+        self.isKing = False
     
     def selectedPiece(self):
         oldrow, oldcol = input("which piece do you try to move?").split()
@@ -33,30 +172,41 @@ class during_game:
         piece = self.board.gameBoard[oldrow][oldcol]
         if piece.lower() == self.color:
             if piece.isupper():
-                isKing = True
+                self.isKing = True
             else:
-                isKing = False
+                self.isKing = False
             self.selected = piece
         else:
+            print("not your color")
             self.selectedPiece()
+            return 1
 
         if self.selected:
-            newrow, newcol = input("where do want the piece move to?").split()
+            self.validMoves = self.board.getValidMoves(self.color, self.isKing, oldrow, oldcol)
+            print("self.validMoves")
+            print(self.validMoves)
+            newrow, newcol = input("where do you want the piece move to?").split()
             newrow, newcol = int(newrow), int(newcol)
-            self.validMoves.append([newrow, newcol])
             result = self.move(oldrow, oldcol, newrow, newcol)
             if not result:
+                print("Faile to move")
                 self.selected = None
                 self.selectedPiece()
-        # else:
-        #     piece = self.board.gameBoard[row][col]
-        #     self.selected = piece
-        #     self.validMoves = self.board.getValidMoves(row, col)
-
 
     def move(self, oldrow, oldcol, newrow, newcol):
-        if self.board.gameBoard[newrow][newcol] == '.' and [newrow, newcol] in self.validMoves:
+        canbemovedb = False
+        for i in self.validMoves:
+            if i[0] == newrow and i[1] == newcol:
+                removepieces = i
+                print("i: ",i)
+                canbemoved = True
+                break
+        if self.board.gameBoard[newrow][newcol] == '.' and canbemoved:
             self.board.move_piece(self.selected, oldrow, oldcol, newrow, newcol, gameBoard)
+            jump = removepieces
+            if jump:
+                self.board.remove(jump)
+            output(self.board.gameBoard)
         else:
             return False
         return True
